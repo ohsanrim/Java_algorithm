bfcfe84e2a6a67497adb57724fc318903855af90
ohsanrim <ka28@naver.com>
2020-03-27 22:30:50 +0900

    [백준] 3053번 : 택시 기하학

---
+++ Beakjoon_practice/Beak_3053.java
@@ -0,0 +1,18 @@
+package Beak_jun;
+
+import java.util.Scanner;
+
+public class Beak_3053 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        double R = sc.nextInt();  //입력받은 반지름
+        double R2=R*R;
+        double PI= 3.14159265358979;
+        double surround = R2*PI;
+        
+        System.out.printf("%.6f\n",surround);
+        System.out.printf("%.6f",2*(R2));
+	}
+
+}
