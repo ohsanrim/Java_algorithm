941ca656a3d5844aa2763e66170fa8b0917f54fe
ohsanrim <ka28@naver.com>
2020-03-28 21:53:07 +0900

    남궁민 선생님의 자바1000제

---
+++ Beakjoon_practice/QuizEx2.java
@@ -0,0 +1,49 @@
+package studyfile;
+
+import java.util.Scanner;
+
+public class QuizEx2 {
+
+	public static void main(String[] args) { 
+        String[] data = { 
+              "다음 중 키워드가 아닌 것은?`2`final`True`if`public", 
+              "다음 중 자바의 연산자가 아닌 것은?`6`&`|`++`!=`/`^", 
+              "다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false`", 
+        }; 
+        
+        Scanner s = new Scanner(System.in); 
+        int count = 0; 
+
+        for(int i=0;i < data.length;i++) { 
+              String[] tmp = data[i].split("`",3); 
+
+              String question = tmp[0]; 
+              String answer = tmp[1]; 
+              String[] choices = tmp[2].split("`"); 
+
+              System.out.println("["+(i+1)+"] "+question); 
+              
+              for(int x=0;x < choices.length;x++) { 
+                    System.out.print((x+1)+"."+choices[x]+"\t"); 
+              } 
+              System.out.println(); 
+              System.out.println("[답]");
+              String input =s.nextLine();
+              
+              if(input.equals(answer)) {
+            	  count++;
+              }
+
+              System.out.println(); 
+              System.out.println(); 
+        } 
+        
+
+        System.out.println("정답개수/전체문항수:"+count+"/"+data.length);
+
+
+  } // main 
+} 
+
+
+
---
+++ Beakjoon_practice/Quiztext.java
@@ -0,0 +1,14 @@
+package studyfile;
+
+public class Quiztext {
+
+	public static void main(String[] args) {
+		int tmp=0;
+		for(int j=0;j<10;j++) {
+		for(int i=0;i<10;i++) {
+			System.out.print(tmp++);
+		}
+		System.out.println();
+		}
+	}}
+
