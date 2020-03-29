2923987ba03db8f4fd8002d9587b540ebd6ca0b0
ohsanrim <ka28@naver.com>
2020-03-29 10:13:29 +0900

    [백준] 2798번: 블랙잭

---
+++ Beakjoon_practice/Beak_2789.java
@@ -0,0 +1,33 @@
+package Beak_Jun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beak_2231 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int max = 0;
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					if (sum <= M && max < sum) {
+						max = sum;
+					}
+				}
+			}
+		}
+		System.out.println(max);
+	}
+}
