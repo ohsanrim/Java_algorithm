e6ad8739630f5dbe8603bee803d05a7388fd5347
ohsanrim <ka28@naver.com>
2020-04-13 23:20:04 +0900

    [CodeUp] 3701: 파스칼 삼각형(재귀함수 연숩)

---
+++ CodeUp/CodeUp_3701.java
@@ -0,0 +1,39 @@
+package Beak_Jun;
+
+import java.util.Scanner;
+
+public class CodeUp_3701 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();  //파스칼의 삼각형을 수현할 줄 개수를 입력하기
+        Pascal(n);
+    }
+
+    public static long [] Pascal(int n) {  //파스칼의 삼각형 출력하는 메소드
+        long[] arr = new long[n];
+        if (n == 1) {  //맨 첫줄
+            arr[0] = 1;
+            System.out.println(arr[0]);
+            return arr;
+        } else if (n == 2) { // 두번째 줄
+            arr[0] = 1;
+            arr[1] = 1;
+            Pascal(n - 1);
+        } else { // 그 외 나머지 줄의 값
+            arr[0] = 1;
+            arr[n - 1] = 1;
+            long[] temp = new long[n - 1];
+            temp = Pascal(n - 1); // 재귀
+            for (int i = 1; i < n - 1; i++) {
+                arr[i] = temp[i - 1] + temp[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        return arr;
+    }
+}
+
