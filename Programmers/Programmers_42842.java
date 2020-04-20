aca88d874092b487900bb14791eeb95106db646b
ohsanrim <ka28@naver.com>
2020-04-20 20:41:01 +0900

    [프로그래머스] 카펫 (완전탐색)

---
+++ Programmers/Programmers_42842.java
@@ -0,0 +1,21 @@
+package git_practice;
+
+public class Programmers_42842 {
+
+	public static void main(String[] args) {
+		int brown = 24;
+		int red = 24;
+		int whole = red + brown;
+		int[] answer = new int[2];
+		for (int i = 3; i < brown - 2; i++) {
+			if (whole % i == 0) {
+				if(red==(i-2)*(whole/i-2)) {
+					answer[1]=i;
+					answer[0]=whole/i;
+					break;
+				}
+			}
+		}
+		System.out.println(answer[0] + " " + answer[1]);
+	}
+}
