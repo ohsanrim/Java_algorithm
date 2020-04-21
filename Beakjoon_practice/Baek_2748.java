b7a7aaf09be480cfe83e674f2d17deac00adf3bc
ohsanrim <ka28@naver.com>
2020-04-21 17:54:18 +0900

    [백준] 2748: 피보나치 수2 (동적 계획법)

--- Beakjoon_practice/Baek_2748.java
+++ Beakjoon_practice/Baek_2748.java
@@ -1,42 +1,29 @@
 package Beak_Jun;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Baek_2748 {
 	public static long[] answer;
-	public static int [] count0;
-	public static int [] count1;
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int number=sc.nextInt();
-		for(int i=0;i<number;i++) {
-			int n = sc.nextInt();
-			answer = new long[n+1];
-			count0=new int[n+1];
-			count1=new int[n+1];
-			long result =Fibonacci(n);
-			System.out.println(count0[n]+" "+count1[n]);
-		}
+		Scanner sc = new Scanner (System.in);
+		int n=sc.nextInt();
+		answer = new long[n+1];
+		long result =Fibonacci(n);
+		System.out.println(result);
 	}
 	public static long Fibonacci(int n) {
 		
 		if(answer[n]!=0) return answer[n];
 		if(n==0) {
 			answer[n]=0;
-			count0[0]=1;
-			count1[0]=0;
 			return answer[n];
 		} else if(n==1) {
 			answer[n]=1;
-			count0[1]=0;
-			count1[1]=1;
 			return answer[n];
 		} else {
 			answer[n]=Fibonacci(n-1)+Fibonacci(n-2);
-			count0[n]=count0[n-1]+count0[n-2];
-			count1[n]=count1[n-1]+count1[n-2];
 			return answer[n];
 		}
 	} 
 }
+
