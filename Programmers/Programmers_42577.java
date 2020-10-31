412434bf6941f879a265b47f4c9498391f0fbe0d
OHrin <ka28@naver.com>
2020-10-31 23:36:38 +0900

    [2020/10/31] 파일 수정

---
+++ Programmers/Programmers_42577.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+public class Programmers_42577 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Programmers/Programmers_42579.java
+++ Programmers/Programmers_42579.java
@@ -1,3 +1,4 @@
+package Programmers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -5,17 +6,14 @@ import java.util.Map;
 public class Programmers_42579 {
 
 	public static void main(String[] args) {
-		
 		String[] genres = {"classic", "pop", "classic", "classic", "pop"};
 		int[] plays = {500, 600, 150, 800, 2500};
 		
-		//HashMap<Integer, String> track = new HashMap<Integer, String>();
 		HashMap<String, Integer> genresCount = new HashMap<String, Integer>();
-		//HashMap<Integer, Integer> playsCount = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> trackPlays = new HashMap<Integer, Integer>();
 		int count = 0;
 		for(int i=0;i<genres.length;i++) {
-			//track.put(i, genres[i]);
-			//playsCount.put(i, plays[i]);
+			trackPlays.put(plays[i],i);
 			if(genresCount.containsKey(genres[i])) {
 				//값이 들어있을 경우
 				genresCount.put(genres[i], genresCount.get(genres[i])+plays[i]);
@@ -25,11 +23,16 @@ public class Programmers_42579 {
 				genresCount.put(genres[i], plays[i]);
 			}
 		}
+		int [] answer= new int[count*2];
+		count=0;
 		//장르에 따른 최대 재생수 넣기
 		ArrayList <Object[]> list = new ArrayList<Object[]>();
 		for(Map.Entry<String,Integer> entry : genresCount.entrySet()){
+			//System.out.println(entry.getKey()+"/"+entry.getValue());
 			Object [] arr = new Object[3];
 			arr[0]=entry.getKey();
+			arr[1]=0;
+			arr[2]=0;
 			for(int j=0;j<plays.length;j++) {
 				if(entry.getKey().equals(genres[j])) {
 					if(plays[j]>(int)arr[1]) { 
@@ -40,9 +43,12 @@ public class Programmers_42579 {
 					}
 				}
 			}
+			answer[count]=trackPlays.get(arr[1]);
+			answer[count+1]=trackPlays.get(arr[2]);
 			
+			System.out.println(answer[count]+","+answer[count+1]);
+			count+=2;
 		}
-
 		
 	}
 
