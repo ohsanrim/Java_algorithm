6f49e0935e906bc2060c49e5af0aefed248ec27b
OHrin <ka28@naver.com>
2021-01-14 22:51:30 +0900

    코드 전면수정(cctv 감시 위치를 3차원 배열에 담아서 새로 풀어보았다)

--- Beak_Jun/Baek_15683.java
+++ Beak_Jun/Baek_15683.java
@@ -8,10 +8,13 @@ import java.util.ArrayList;
 public class Baek_15683 {
 	static int n, m;
 	static int[][] map;
-	static ArrayList<Point> list;
+	static ArrayList<int[]> list;
 	static int min = Integer.MAX_VALUE;
 	static boolean[][] cctv;
 	static int[][] move = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
+	static int [][][] cctvMove= {{{0},{1},{2},{3}},{{0,2},{1,3}},{{0,1},{1,2},{2,3},{3,0}},
+			{{0,1,2},{1,2,3},{0,2,3},{0,1,3}},{{0,1,2,3}}};
+	
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -30,14 +33,13 @@ public class Baek_15683 {
 				if (map[i][j] != 0)
 					cnt++;  
 				if (map[i][j] >= 1 && map[i][j] <= 5) {
-					list.add(new Point(i, j, map[i][j]));
+					list.add(new int[] {i,j,map[i][j]-1});
 				}
 			}
 		}
 		dfs(0, 0);
 		System.out.println(min - cnt);
 	}
-
 	private static void dfs(int level, int cal) {
 		if (level == list.size()) {
 			// 끝났을 경우
@@ -46,44 +48,29 @@ public class Baek_15683 {
 		}
 		//시시티비 계산 시작
 		boolean[][] tempV = new boolean[n][m];
-		tempV = copy(tempV);
-		int calTemp = 0;
-		Point p = list.get(level);
-		if (p.pos == 2) {  //2번 cctv
-			for (int j = 0; j < 2; j++) {
-				calTemp = watch(p.x, p.y, move[j][0], move[j][1]) + watch(p.x, p.y, move[j + 2][0], move[j + 2][1]);
-				dfs(level + 1, cal + calTemp);
-				rollback(tempV);
-			}
-			return;
-		} else if (p.pos == 5) {  //5번 cctv
-			for (int i = 0; i < 4; i++) {
-				calTemp += watch(p.x, p.y, move[i][0], move[i][1]);
+		copy(tempV);
+		int [] point = list.get(level);
+		int x=point[0];
+		int y=point[1];
+		int pos = point[2];
+		for(int i=0;i<cctvMove[pos].length;i++) {
+			int calTemp = 0;
+			for(int j=0;j<cctvMove[pos][i].length;j++) {
+				calTemp+=watch(x,y,move[cctvMove[pos][i][j]][0], move[cctvMove[pos][i][j]][1]);
 			}
 			dfs(level + 1, cal + calTemp);
 			rollback(tempV);
-			return;
-		} else {
-			for (int i = 0; i < 4; i++) {
-				calTemp = 0;
-				if (p.pos == 1) {  //1번 cctv 
-					calTemp = watch(p.x, p.y, move[i][0], move[i][1]);
-				} else if (p.pos == 3) {  //3번 cctv 
-					calTemp = watch(p.x, p.y, move[i][0], move[i][1])
-							+ watch(p.x, p.y, move[i == 3 ? 0 : i + 1][0], move[i == 3 ? 0 : i + 1][1]);
-				} else if (p.pos == 4) {  //4번 cctv 
-					for (int j = 0; j < 4; j++) {
-						if (i != j) {
-							calTemp += watch(p.x, p.y, move[j][0], move[j][1]);
-						}
-					}
-				}
-				dfs(level + 1, cal + calTemp);
-				rollback(tempV);
+		}
+	}
+	
+	private static void copy(boolean[][] tempV) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				tempV[i][j] = cctv[i][j];
 			}
 		}
 	}
-
+	
 	private static void rollback(boolean[][] tempV) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
@@ -94,42 +81,18 @@ public class Baek_15683 {
 
 	private static int watch(int x, int y, int i, int j) {
 		int cnt = 0;
-		while (true) {
-			x += i;
-			y += j;
-			if (!isIn(x, y) || map[x][y] == 6)
-				break;
+		while (isIn(x, y) && map[x][y] != 6) {
 			if (!cctv[x][y]) {
 				cctv[x][y] = true;
 				if (map[x][y] == 0)
 					cnt++;
 			}
-
+			x += i;
+			y += j;
 		}
 		return cnt;
 	}
-
 	private static boolean isIn(int x, int y) {
 		return x >= 0 && y >= 0 && x < n && y < m;
-	}
-
-	private static boolean[][] copy(boolean[][] tempV) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				tempV[i][j] = cctv[i][j];
-			}
-		}
-		return tempV;
-	}
-
-	private static class Point {
-		int x, y;
-		int pos; // 몇개의 면을 감시해야 하는지
-
-		Point(int x, int y, int pos) {
-			this.x = x;
-			this.y = y;
-			this.pos = pos;
-		}
 	}
 }
